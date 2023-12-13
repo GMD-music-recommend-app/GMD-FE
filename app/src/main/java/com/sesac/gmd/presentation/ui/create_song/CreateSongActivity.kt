1a10287c8974a16bc730a3a2aa39b1e044848d76
jinsuCH0 <blacksky9795@naver.com>
2023-12-13 22:54:05 +0900

    test commit 2

--- app/src/main/java/com/sesac/gmd/presentation/ui/create_song/CreateSongActivity.kt
+++ app/src/main/java/com/sesac/gmd/presentation/ui/create_song/CreateSongActivity.kt
@@ -2,11 +2,15 @@ package com.sesac.gmd.presentation.ui.create_song
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sesac.gmd.R
+import com.sesac.gmd.databinding.ActivityCreateSongBinding
 
 class CreateSongActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateSongBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_song)
+        binding = ActivityCreateSongBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }
