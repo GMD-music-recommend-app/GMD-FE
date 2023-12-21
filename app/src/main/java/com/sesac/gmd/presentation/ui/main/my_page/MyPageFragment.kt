9481cb74b231dcda359519ead65b1c6e66e9d007
jscho3618 <jscho3618@raoncorp.com>
2023-12-21 19:09:44 +0900

    Fix : 마이페이지 화면 잘리는 문제 수정

--- app/src/main/java/com/sesac/gmd/presentation/ui/main/my_page/MyPageFragment.kt
+++ app/src/main/java/com/sesac/gmd/presentation/ui/main/my_page/MyPageFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import com.sesac.gmd.R
-import com.sesac.gmd.databinding.FragmentSettingBinding
+import com.sesac.gmd.databinding.FragmentMyPageBinding
 import com.sesac.gmd.presentation.base.BaseFragment
 
-class MyPageFragment : BaseFragment<FragmentSettingBinding>() {
-    override val layoutResourceId = R.layout.fragment_setting
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>() {
+    override val layoutResourceId = R.layout.fragment_my_page
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
